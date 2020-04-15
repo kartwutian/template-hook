f47aac0dc8da0a36db9473eeef82aae3d8988feb
baohz <baohz@zjbdos.com>
2020-04-15 16:37:34 +0800

    add prettier script

--- src/_router.js
+++ src/_router.js
@@ -2,39 +2,39 @@
 
 const router = [
   {
-    "route": "/login",
-    "routes": [],
-    "path": "pages/Login/index",
-    "name": "登录",
-    "isInLayout": false
+    route: '/login',
+    routes: [],
+    path: 'pages/Login/index',
+    name: '登录',
+    isInLayout: false,
   },
   {
-    "route": "/home",
-    "routes": [],
-    "path": "pages/Home/index",
-    "name": "首页"
+    route: '/home',
+    routes: [],
+    path: 'pages/Home/index',
+    name: '首页',
   },
   {
-    "route": "/project",
-    "routes": [
+    route: '/project',
+    routes: [
       {
-        "route": "/project/list",
-        "routes": [],
-        "path": "pages/Project/List",
-        "name": "项目列表",
-        "hasBread": true
+        route: '/project/list',
+        routes: [],
+        path: 'pages/Project/List',
+        name: '项目列表',
+        hasBread: true,
       },
       {
-        "route": "/project/Developing",
-        "routes": [],
-        "path": "pages/Project/Developing",
-        "name": "项目开发",
-        "template": "list",
-        "hasBread": true
-      }
+        route: '/project/Developing',
+        routes: [],
+        path: 'pages/Project/Developing',
+        name: '项目开发',
+        template: 'list',
+        hasBread: true,
+      },
     ],
-    "name": "项目"
-  }
-]
+    name: '项目',
+  },
+];
 
 export default router;
